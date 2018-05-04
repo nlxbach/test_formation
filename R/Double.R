fe5a59c200070472de57f7cf5d376b2a85c48919
nlxbach <nlxbach@gmail.com>
2018-05-04 16:01:54 -0300

    FuzzyDouble

--- R/Double.R
+++ R/Double.R
@@ -17,3 +17,59 @@ Double <- function(number) {
     stop("Double requires a numeric object")
   return(2L * number)
 }
+
+
+
+#' FuzzyDouble
+#'
+#' Fuzzy double of a numeric object.
+#'
+#' Doubles an object with a random noise: a Gaussian error drawn
+#' by \code{\link{rnorm}}.
+#'
+#' @param x A numeric object
+#' @param mean The mean noise. Default is 0.
+#' @param sd The standard deviation of the noise. Default is 1.
+#'
+#' @return a \code{FuzzyDouble} object which is a data.frame with
+#' columns \code{x} for the input and \code{y} for the output.
+#'
+#' @seealso \code{\link{plot.FuzzyDouble}},
+#' \code{\link{autoplot.FuzzyDouble}}
+#' @export
+FuzzyDouble <- function(x, mean = 0, sd = 1) {
+  # Double x and add normal error
+  y <- 2 * x + stats::rnorm(n = length(x), mean = mean,
+                            sd = sd)
+  # Make a data.frame
+  fuzzydouble <- data.frame(x = x, y = y)
+  # Make it a FuzzyMultiple object
+  class(fuzzydouble) <- c("FuzzyDouble", class(fuzzydouble))
+  return(fuzzydouble)
+}
+
+
+#' Plot FuzzyDouble
+#'
+#' Plot a FuzzyDouble object
+#'
+#' @param x The \code{\link{FuzzyDouble}} object
+#' @param xlab The X-axis label
+#' @param ylab The Y-axis label
+#' @param ... Extra parameters passed to \code{\link{plot}}
+#' @param LineCol The color of the line representing $y=2x$
+#'
+#' @importFrom graphics plot
+#' @method plot FuzzyDouble
+#' @export
+#'
+#' @examples
+#' plot(FuzzyDouble(1:10))
+plot.FuzzyDouble <- function(x, xlab = "x", ylab = "Double",
+                             ..., LineCol = "red") {
+  # xy standard plot
+  graphics::plot(x$x, x$y, xlab = xlab, ylab = ylab,
+                 ...)
+  # Add the regression line
+  graphics::lines(x$x, 2 * x$x, col = LineCol)
+}
